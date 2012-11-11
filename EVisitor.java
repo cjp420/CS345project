893e81ef9045642b623276fbac836fb4fc962291
Noah Gregg <blaster.rifle@gmail.com>
2012-11-11 16:43:29 -0600

    some of the files I originally added were not saved

---
+++ EVisitor.java
@@ -0,0 +1,43 @@
+public class EVisitor implements ParserVisitor
+{
+  public Object visit(SimpleNode node, Object data)
+  {
+    //This is an error
+  }
+  public Object visit(ASTinteger node, Object data)
+  {
+
+  }
+  public Object visit(ASTsymbol node, Object data)
+  {
+    
+  }
+  public Object visit(ASTAddition node, Object data)
+  {
+    
+  }
+  public Object visit(ASTSubtraction node, Object data)
+  {
+    
+  }
+  public Object visit(ASTMultiplication node, Object data)
+  {
+    
+  }
+  public Object visit(ASTDivision node, Object data)
+  {
+    
+  }
+  public Object visit(ASTFunctionApplication node, Object data)
+  {
+    
+  }
+  public Object visit(ASTFunction node, Object data)
+  {
+    
+  }
+  public Object visit(ASTprog node, Object data)
+  {
+    
+  }
+}
