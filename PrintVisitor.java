bb85232c3237cad0b4c8495d2acf963aae61fab8
Noah Gregg <blaster.rifle@gmail.com>
2012-11-11 16:24:05 -0600

    current set of java file uploaded.  No Class files uploaded as they can be built.

---
+++ PrintVisitor.java
@@ -0,0 +1,86 @@
+public class PrintVisitor implements ParserVisitor
+{
+  private int indent = 0;
+
+  private String indentString() {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < indent; ++i) {
+      sb.append(' ');
+    }
+    return sb.toString();
+  }
+
+  public Object visit(SimpleNode node, Object data) {
+    System.out.println(indentString() + node +
+                   ": acceptor not unimplemented in subclass?");
+    ++indent;
+    data = node.childrenAccept(this, data);
+    --indent;
+    return data;
+  }
+
+  public Object visit(ASTinteger node, Object data){
+System.out.println(indentString() + node);
+++indent;
+data = node.childrenAccept(this, data);
+--indent;
+return data;
+}
+  public Object visit(ASTsymbol node, Object data){
+System.out.println(indentString() + node);
+++indent;
+data = node.childrenAccept(this, data);
+--indent;
+return data;
+}
+  public Object visit(ASTAddition node, Object data){
+System.out.println(indentString() + node);
+++indent;
+data = node.childrenAccept(this, data);
+--indent;
+return data;
+}
+  public Object visit(ASTSubtraction node, Object data){
+System.out.println(indentString() + node);
+++indent;
+data = node.childrenAccept(this, data);
+--indent;
+return data;
+}
+  public Object visit(ASTMultiplication node, Object data){
+System.out.println(indentString() + node);
+++indent;
+data = node.childrenAccept(this, data);
+--indent;
+return data;
+}
+  public Object visit(ASTDivision node, Object data){
+System.out.println(indentString() + node);
+++indent;
+data = node.childrenAccept(this, data);
+--indent;
+return data;
+}
+  public Object visit(ASTFunctionApplication node, Object data){
+System.out.println(indentString() + node);
+++indent;
+data = node.childrenAccept(this, data);
+--indent;
+return data;
+}
+  public Object visit(ASTFunction node, Object data){
+System.out.println(indentString() + node);
+++indent;
+data = node.childrenAccept(this, data);
+--indent;
+return data;
+}
+  public Object visit(ASTprog node, Object data){
+System.out.println(indentString() + node);
+++indent;
+data = node.childrenAccept(this, data);
+--indent;
+return data;
+}
+}
+
